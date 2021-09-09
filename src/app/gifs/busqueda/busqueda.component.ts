1943c94a6bdeb2a6f2f6c5c482c33a96d1f3e0c4
Albeiro Marin <xlandru@hotmail.com>
2021-09-09 09:58:27 -0500

    Lee el valor del input de busqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: 'busqueda.component.html',
 })
-export class BusquedaComponent {}
+export class BusquedaComponent {
+
+  //buscando un elemento en el dom, parecido al useRef() de react
+  @ViewChild("txtBuscar") txtBuscar!:ElementRef<HTMLInputElement>;
+
+
+  buscar():void{
+    const inputValue = this.txtBuscar.nativeElement.value;
+    this.txtBuscar.nativeElement.value = "";
+  }
+}
